6eb14be05cfd2a4db162acc8495e9adbe4eb383d
Matheus Duarte <mtduarteadm@hotmail.com>
2021-03-25 17:46:40 -0300

    Respostas da Atividade Remota - Semana 02 - Tarefa 1
    Respostas da Atividade Remota - Semana 02 - Tarefa 1

--- Semana 02/animal.py
+++ Semana 02/animal.py
@@ -21,36 +21,30 @@ def cadastrar(animais: Animal):
     return animais
 
 
-
 @app.get('/animais')
 def lista_animais():
     return bd_animais
 
 
-
 @app.get('/animais/{id}')
 def animal_id(id: str):
-    indice = -1
-    for i, animais in enumerate(bd_animais):
-        if animais.id == indice:
-            indice = i
-            break
-    return {'mensagem': f'{animais.nome} localizado com sucesso!.'}
-
+    for animal in bd_animais:
+        if animal.id == id:
+            return {'mensagem': f'{animal.nome} localizado com sucesso!.'}
 
 
 @app.delete('/animais/{id}')
 def remover_animais(id: str):
     ia = -1
-    for i, animais in enumerate(bd_animais):
-        print(animais.id, id)
-        if animais.id == id:
+    for i, animal in enumerate(bd_animais):
+        print(animal.id, id)
+        if animal.id == id:
             ia = i
             break
 
     if ia != -1:
         bd_animais.pop(ia)
-        return {'mensagem': f'Animal {animais.nome} removido com sucesso!'}
+        return {'mensagem': f'Animal {animal.nome} removido com sucesso!'}
     else:
         return {'mensagem': f'Não foi possível localizar o animal com o id {id}'}
 
