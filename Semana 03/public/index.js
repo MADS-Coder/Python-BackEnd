32f77062669ff305cf763af8bd8f9bd67db583ca
Matheus Duarte <mtduarteadm@hotmail.com>
2021-04-11 14:25:44 -0300

    App Web consumindo a API com Javascript
    App Web consumindo a API com Javascript

--- Semana 03/public/index.js
+++ Semana 03/public/index.js
@@ -10,7 +10,7 @@ async function carregarAnimais() {
     animais.forEach(animal => {
         const item = document.createElement('li')
 
-        const linha = `${animal.nome} - idade: ${animal.idade} - cor: ${animal.cor}`
+        const linha = `${animal.nome} - idade: ${animal.idade} - cor: ${animal.cor} - sexo: ${animal.sexo}`
 
         item.innerText = linha
 
@@ -22,23 +22,36 @@ async function carregarAnimais() {
 
 }
 
+
+String.prototype.capitalize = function() {
+	return this.charAt(0).toUpperCase() + this.substr(1);
+}
+
 function manipularFormulario() {
     const form_animal = document.getElementById('form-animal')
     const input_nome = document.getElementById('nome')
+    const input_idade = document.getElementById('idade')
+    const input_cor = document.getElementById('cor')
+    
 
     form_animal.onsubmit = async (event) => {
         event.preventDefault()
-        const nome_animal = input_nome.value
+        const nome_animal = input_nome.value.capitalize()
+        const idade_animal = input_idade.value
+        const cor_animal = input_cor.value.capitalize()
+        const sexo_animal = document.querySelector('input[name="opcao"]:checked').value
+        
 
         await axios.post('http://localhost:8000/animais', {
             nome: nome_animal,
-            idade: 4,
-            sexo: 'macho',
-            cor: 'preto'
+            idade: idade_animal,
+            sexo: sexo_animal,
+            cor: cor_animal
         })
 
         carregarAnimais()
         alert('Animal cadastrado..')
+        document.querySelector('form').reset()
     }
 }
 
@@ -46,6 +59,8 @@ function app() {
     console.log('App iniciada')
     carregarAnimais()
     manipularFormulario()
+    capitalize()
+
 }
 
 app()
